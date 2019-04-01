6ec85f48e33d79fe76d75e8b41043a7143f5bf9c
fpsstyle228 <fpsstyle228@gmail.com>
2019-04-01 11:36:30 +0300

    fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+// Components
 import { AppComponent } from './app.component';
 
 @NgModule({
