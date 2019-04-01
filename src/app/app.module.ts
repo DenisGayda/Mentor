e5c00fafdceb65778b2e75dba3c32dd06e60145e
fpsstyle228 <fpsstyle228@gmail.com>
2019-04-01 12:09:03 +0300

    1.0.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AuthorizationComponent } from './authorization/authorization.component';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
     declarations: [
         AppComponent,
+        AuthorizationComponent,
     ],
     imports: [
         BrowserModule,
+        ReactiveFormsModule,
     ],
     providers: [],
     bootstrap: [AppComponent],
---
+++ src/app/authorization/authorization.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+    selector: 'app-authorization',
+    templateUrl: './authorization.component.html',
+    styleUrls: ['./authorization.component.css'],
+})
+export class AuthorizationComponent implements OnInit {
+    public authFormGroup: FormGroup;
+
+    constructor(private fb: FormBuilder) { }
+
+    ngOnInit() {
+        this.authFormGroup = this.fb.group({
+          email: ['', [Validators.required, Validators.email]],
+            password: ['', [Validators.required]],
+      });
+    }
+
+}
