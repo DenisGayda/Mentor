5a70f3a61324e1d0d0a978238a610952f310c281
fpsstyle228 <fpsstyle228@gmail.com>
2019-04-01 11:09:19 +0300

    Added Angular Material
    -

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material';
+import { AuthComponent } from './auth/auth.component';
 
 @NgModule({
     declarations: [
         AppComponent,
+        AuthComponent,
     ],
     imports: [
         BrowserModule,
+        BrowserAnimationsModule,
+        MatButtonModule,
     ],
     providers: [],
     bootstrap: [AppComponent],
