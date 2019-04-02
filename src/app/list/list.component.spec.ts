badc78e05a6b32560f688da34e490b44c8ee1b62
danilSnegurov <snegurov09mail.ru@gmail.com>
2019-04-02 17:50:38 +0300

    corrections edition

--- src/app/list/list.component.spec.ts
+++ src/app/list/list.component.spec.ts
@@ -1,7 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ListComponent } from './list.component';
-
 describe('ListComponent', () => {
   let component: ListComponent;
   let fixture: ComponentFixture<ListComponent>;
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,9 +1,9 @@
 import { Component, Input, ChangeDetectionStrategy, TemplateRef } from '@angular/core';
 @Component({
   selector: 'app-list',
-  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  styleUrls: ['./list.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ListComponent {
   @Input() items: Array<any>;
