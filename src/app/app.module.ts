37f8c54667ba7f6ba239b48501d17df95eba0694
danilSnegurov <snegurov09mail.ru@gmail.com>
2019-04-03 15:41:54 +0300

    nc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-import { ListComponent } from './list/list.component';
 
 @NgModule({
     declarations: [
         AppComponent,
-        ListComponent,
     ],
     imports: [
         BrowserModule,
