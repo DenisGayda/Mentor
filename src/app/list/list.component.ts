7364b243231daff9f7fcaab0f1c2caae196a9a0b
danilSnegurov <snegurov09mail.ru@gmail.com>
2019-04-03 10:52:41 +0300

    prod

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,11 +1,11 @@
 import { Component, Input, ChangeDetectionStrategy, TemplateRef } from '@angular/core';
 @Component({
-  selector: 'app-list',
-  templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+    selector: 'app-list',
+    templateUrl: './list.component.html',
+    styleUrls: ['./list.component.css'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ListComponent {
-  @Input() items: Array<any>;
-  @Input() itemTemplate: TemplateRef<any>;
+export class ListComponent<T> {
+    @Input() items: T[];
+    @Input() itemTemplate: TemplateRef<T>;
 }
