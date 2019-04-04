7d4c673779b95c4dbc88fb1c8051bf68a10f4eaf
Ilya Tsapu <sonofzeus2205@gmail.com>
2019-04-04 13:35:01 +0300

    rename

---
+++ src/app/components/mentor/mentor-page-module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { MentorPageComponent } from './mentor-page-component';
+
+@NgModule({
+    declarations: [MentorPageComponent],
+    exports: [MentorPageComponent],
+})
+export class MentorPageModule { }
