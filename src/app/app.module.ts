6b5e823370dd0ba4fb370641b21f5d644c0dc96f
fpsstyle228 <fpsstyle228@gmail.com>
2019-04-04 11:33:54 +0300

    View done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
+import {MatButtonModule, MatCardModule, MatDividerModule, MatIconModule, MatInputModule, MatSelectModule} from '@angular/material';
 
 @NgModule({
     declarations: [
         AppComponent,
+        TestComponent,
     ],
     imports: [
         BrowserModule,
         BrowserAnimationsModule,
+        MatSelectModule,
+        MatDividerModule,
+        MatInputModule,
+        MatCardModule,
+        MatButtonModule,
+        MatIconModule,
     ],
     providers: [],
     bootstrap: [AppComponent],
