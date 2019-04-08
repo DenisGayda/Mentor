fc465937b059c4fcc9ca505f5ca07501273eacef
danilSnegurov <snegurov09mail.ru@gmail.com>
2019-04-08 11:27:46 +0300

    +css

--- src/app/components/add-user/add-user.component.ts
+++ src/app/components/add-user/add-user.component.ts
@@ -13,7 +13,11 @@ export class AddUserComponent implements OnInit {
     @Output() onAdd = new EventEmitter();
     public user: AddUserInterface;
     public form: FormGroup;
-
+    form = new FormGroup({
+        firstName: new FormControl('', Validators.required),
+        lastName: new FormControl('', Validators.required),
+        email: new FormControl('', Validators.pattern('[a-zA-Z0-9_]+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}')),
+    });
     public addUser(): void {
         this.user = {
             name: this.form.value.firstName,
@@ -26,11 +30,7 @@ export class AddUserComponent implements OnInit {
     }
 
     ngOnInit(): void {
-        this.form = new FormGroup({
-            firstName: new FormControl('', Validators.required),
-            lastName: new FormControl('', Validators.required),
-            email: new FormControl('', Validators.pattern('[a-zA-Z0-9_]+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}')),
-        });
+
     }
 
 }
--- src/app/components/add-user/add-user.module.ts
+++ src/app/components/add-user/add-user.module.ts
@@ -3,8 +3,7 @@ import { CommonModule } from '@angular/common';
 import { AddUserComponent } from './add-user.component';
 import { MatButtonModule } from '@angular/material';
 import { MatInputModule } from '@angular/material/input';
-import { FormsModule } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
     imports: [
