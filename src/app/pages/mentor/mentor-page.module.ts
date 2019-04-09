69dd9d6c7b779c457344aa8e6aabdf3123063706
danilSnegurov <snegurov09mail.ru@gmail.com>
2019-04-09 15:51:40 +0300

    corr

--- src/app/pages/mentor/mentor-page.module.ts
+++ src/app/pages/mentor/mentor-page.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { MentorPageComponent } from './mentor-page.component';
 import { MentorPageRouterModule } from './mentor-page-router.module';
-import { AddUserModule } from '../../components/add-user/add-user.module';
 import { LearnCardModule } from '../../components/learn-card/learn-card.module';
 @NgModule({
-    imports: [ MentorPageRouterModule, LearnCardModule, AddUserModule ],
+    imports: [ MentorPageRouterModule, LearnCardModule ],
     declarations: [ MentorPageComponent ],
 })
 export class MentorPageModule { }
