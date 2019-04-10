3149a45f1e111a4cd88450dda0f1a916808db7b4
fpsstyle228 <fpsstyle228@gmail.com>
2019-04-10 16:53:57 +0300

    Update auth.service.spec.ts

--- src/app/services/AuthService/auth.service.spec.ts
+++ src/app/services/AuthService/auth.service.spec.ts
@@ -1,6 +1,5 @@
 import { TestBed } from '@angular/core/testing';
 import { AuthService } from './auth.service';
-import { AngularFireAuth } from '@angular/fire/auth';
 import { instance, mock } from 'ts-mockito';
 import { CloudStoreService } from '../CloudStoreService/cloud-store.service';
 import { HttpClientModule } from '@angular/common/http';
@@ -9,7 +8,6 @@ describe('AuthService', () => {
     beforeEach(() => TestBed.configureTestingModule({
         imports: [ HttpClientModule ],
         providers: [
-            { provide: AngularFireAuth, useFactory: () => instance(mock(AngularFireAuth)) },
             { provide: CloudStoreService, useFactory: () => instance(mock(CloudStoreService)) },
             AuthService,
         ],
